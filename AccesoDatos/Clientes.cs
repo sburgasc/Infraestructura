9e03f12f4e46a11eeb2270f68c1da698e8a7ae00
SBurga <sburga@sintad.net>
2017-08-17 18:24:49 -0500

    Agregue Metodo Listar Cliente

--- AccesoDatos/Clientes.cs
+++ AccesoDatos/Clientes.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using Entidades;
 
 public class Clientes
 {
-	public Clientes()
-	{
+	
+        public List<ClienteE> listarClientes()
+        {
+            List<ClienteE> cliente = new List<ClienteE>();
 
-	}
+            using (MySqlConnection cn = new MySqlConnection(ConfigurationManager.ConnectionStrings["ADOConnectionString"].ToString()))
+            {
+
+
+            }
+            return cliente;
+        }
+
+    
 }
